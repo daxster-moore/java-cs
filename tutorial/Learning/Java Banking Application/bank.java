bad514d81c542468d27038268a66205518404705
Business Man <daxster.moore@gmail.com>
2020-08-18 10:57:52 -0700

    bank and learning commit

---
+++ tutorial/Learning/Java Banking Application/bank.java
@@ -0,0 +1,16 @@
+import java.util.function.Function;
+
+public class bank {
+    public static void mainScreen(String[] args) {
+            System.out.println("**************************");
+            System.out.println("|\tJAVA BANK\t|");
+            System.out.println("**************************");
+            
+            
+            System.out.println("CHEQUING: ");
+            System.out.println("SAVINGS: ")
+            System.out.println("\nTOTAL FUNDS: ");
+
+        // TODO: Add option to add chequing, savings. Once added, it takes you back to the main screen
+    }
+}
---
+++ tutorial/Learning/conditionals_and_loops.java
@@ -0,0 +1,53 @@
+// import java.util.Scanner;
+
+public class conditionals_and_loops {
+    public static void main(String[] args) {
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("What day is it? ");
+        // String dayInput = input.nextLine();
+        // String upperCase = dayInput.substring(0, 1).toUpperCase() + dayInput.substring(1);
+        // switch (upperCase) {
+        //   case "Monday":
+        //     System.out.println("Today is: " + upperCase);
+        //     break;
+
+        //   case "Tuesday":
+        //     System.out.println("Today is: " + upperCase);
+        //     break;
+
+        //   case "Wednesday":
+        //     System.out.println("Today is: " + upperCase);
+        //     break;
+
+        //   case "Thursday":
+        //     System.out.println("Today is: " + upperCase);
+        //     break;
+
+        //   case "Friday":
+        //     System.out.println("Today is: " + upperCase);
+        //     break;
+
+        //   case "Saturday":
+        //     System.out.println("Today is: " + upperCase);
+        //     break;
+
+        //   case "Sunday":
+        //     System.out.println("Today is: " + upperCase);
+        //     break;
+
+        //   default:
+        //     System.out.println("Please enter a proper day. e.g. monday");
+        // }
+        // input.close();
+
+        // int num = 0;
+        // do {
+        //     System.out.println(num);
+        //     num++;
+        // } while (num < 40);
+
+        for (int i = 0; i < 6; i++) {
+            System.out.println(i);
+        }
+    }
+}
